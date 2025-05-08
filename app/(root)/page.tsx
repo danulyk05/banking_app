3e1055446ba5283fd372e71e4894bfd048821737
Ivan Danulyk <ivandanulyk@MacBook-Pro-Ivan.local>
2025-05-08 16:56:07 -0400

    add header and chart with some info about total accounts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,31 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
-const Home  = () => {
-    return(
-        <div className="bg-red-500 ">
-            <h1 className=" font-bold underline ">
-                Home
-            </h1>
-        </div>
+const Home = () => {
+    const loggedIn = { firstName: "Ivan" };
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn.firstName || "Guest"}
+                        subtext="Access & manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox 
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+
+
+            </div>
+        </section>
     )
 }
 
