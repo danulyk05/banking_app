d1f73aa29e58c531dc4aceec7beaeb3c3f5cc97f
Ivan Danulyk <ivandanulyk@MacBook-Pro-Ivan.local>
2025-05-08 21:24:25 -0400

    add mobile navigation and improve left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -11,7 +13,22 @@ export default function RootLayout({
     return (
         <main className=" flex h-screen w-full font-inter">
             <Sidebar user={loggedIn.firstName} /> 
-            {children}
+            <div className=" flex size-full flex-col">
+                <div className="root-layout">
+                    <Image 
+                        src="/icons/logo.svg"
+                        alt="logo"
+                        width={30}
+                        height={30}
+                    />
+
+                    <div>
+                        <MobileNav user={loggedIn} />
+                    </div>
+                </div>
+
+                {children}
+            </div>
         </main>
     );
 }
