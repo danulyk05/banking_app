1108db1b0c168f8acc777c31fd80b2be3cd80154
Ivan Danulyk <ivandanulyk@MacBook-Pro-Ivan.local>
2025-05-08 20:41:58 -0400

    add navigation UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const loggedIn = {firstName: 'Ivan', lastName: 'Danyliuk'}
+
     return (
-        <main>
-            SIDEBAR 
+        <main className=" flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn.firstName} /> 
             {children}
         </main>
     );
