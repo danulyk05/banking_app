351600e568ad97379fce4037d9a106ffa425bfed
Ivan Danulyk <ivandanulyk@MacBook-Pro-Ivan.local>
2025-05-15 19:35:54 -0400

    add sign in and sign up UI

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+
+export const signIn = async (data: { email: string; password: string }) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error', error)
+    }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        
+    } catch (error) {
+        console.log('Error', error)
+    }
+};
